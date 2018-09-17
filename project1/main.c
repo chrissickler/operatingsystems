d6e93b1373e183334ab78db0c674cc9e4c853c79
chrissickler <christopher.sickler@utexas.edu>
2018-09-17 00:54:51 -0500

    chicken

---
+++ project1/main.c
@@ -0,0 +1,11 @@
+/* Christopher Sickler */
+/* cbs2468 */
+/* imports go here */
+
+int main() {
+	printf("Welcome to YASH xD/n");
+	while(1) {
+		printf("$ ");
+		//detect input here and parse it??
+	}
+}
